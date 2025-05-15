3a493c0abb9ace527899072421382f186c2e073c
unknown <arun1anim@gmail.com>
2025-05-15 11:37:17 +0930

    Camera Checking

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    if (navigator.xr) {
+      navigator.xr.isSessionSupported("immersive-ar").then((supported) => {
+        console.log("WebXR AR supported:", supported);
+        if (!supported) {
+          alert("WebXR AR not supported on this device.");
+        }
+      });
+    } else {
+      alert("WebXR not available in this browser.");
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <a-scene
+      embedded
+      vr-mode-ui="enabled: true"
+      renderer="logarithmicDepthBuffer: true;"
+      style={{ width: "100%", height: "100%", position: "fixed", top: 0, left: 0 }}
+    >
+      <a-box position="0 0 -2" color="blue" scale="0.5 0.5 0.5"></a-box>
+      <a-light type="ambient" intensity="1"></a-light>
+      <a-entity camera look-controls></a-entity>
+    </a-scene>
   );
 }
 
